6a0e39b42c143fa92211bb5134489069a831a9a7
yolisses <iamurise@gmail.com>
2023-09-02 17:45:54 -0300

    add get random number

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
+use rand::Rng;
 use std::io::stdin;
 
 fn main() {
     println!("Guess the number!");
+    let number = rand::thread_rng().gen_range(0..=100);
+    println!("The random number is {number}");
+
     println!("Please input your guess.");
     let mut guess = String::new();
     stdin().read_line(&mut guess).expect("Failed to get input");
