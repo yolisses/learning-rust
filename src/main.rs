39017462d3acdb52e5e3abecf5d95473ed7d3b85
yolisses <iamurise@gmail.com>
2023-09-02 18:02:30 -0300

    add random number comparison

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
 use rand::Rng;
-use std::io::stdin;
+use std::{cmp::Ordering, io::stdin};
 
 fn main() {
     println!("Guess the number!");
-    let number = rand::thread_rng().gen_range(0..=100);
-    println!("The random number is {number}");
+    let random_number = rand::thread_rng().gen_range(0..=100);
+    println!("The random number is {random_number}");
 
     println!("Please input your guess.");
     let mut guess = String::new();
     stdin().read_line(&mut guess).expect("Failed to get input");
+    let guess: u32 = guess.trim().parse().expect("Failed to convert to number");
     println!("Your guess was {guess}");
+
+    match guess.cmp(&random_number) {
+        Ordering::Less => println!("Too low!"),
+        Ordering::Greater => println!("Too high!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
