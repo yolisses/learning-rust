bbec2da2ade1a07d8490d6162d0f551e65f376ef
yolisses <iamurise@gmail.com>
2023-09-02 16:13:09 -0300

    add ferris::say

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::io::{stdout, BufWriter};
+
+use ferris_says::say;
+
 fn main() {
-    println!("Hello, world!");
+    let stdout = stdout();
+    let message = String::from("Hellow fellow Rustaceans!");
+    let width = message.chars().count();
+    let mut writer = BufWriter::new(stdout.lock());
+    say(&message, width, &mut writer).unwrap();
 }
